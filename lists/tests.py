4910ecdb91dacd7e6ad3890068b108371e31e03a
sergiopassos <sergio.passos@users.noreply.github.com>
2019-04-16 22:24:39 -0300

    Fizemos nosso primeiro migrate...

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 # from django.urls import resolve
 from django.test import TestCase
+
+from lists.models import Item
 # from django.http import HttpRequest
 
 # from django.template.loader import render_to_string
@@ -35,5 +37,26 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text': 'A new list item'})
-		self.AssertIn('A new list item', response.content.decode())
+		self.assertEqual(Item.objects.count(), 1)
+		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
