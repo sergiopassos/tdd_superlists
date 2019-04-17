5884ffbd59391bbd30321b758f0834887ae3c534
sergiopassos <sergio.passos@users.noreply.github.com>
2019-04-16 21:21:44 -0300

    Fizemos algumas mudanças nos testes, mas ainda não conseguimos lidar com dois itens da lista :(

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Edith ouviu falar de uma nova aplicação online interessante para
@@ -40,14 +45,12 @@ class NewVisitorTest(unittest.TestCase):
 		# Quando ela tecla enter, a página é atualizada, e agora a página lista
 		# "1: Buy peacock feathers" como um item em uma lista de tarefas
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(5)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		time.sleep(2)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		# self.assertTrue(
 		# 	any(row.text == '1: Buy peacock feathers' for row in rows),
 		# 	f"New to-do item did not appear in table. Content were:\n{table.text}"
 		# 	)
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
 
 		# Ainda continua havendo uma caixa de texto convidando-a a acrescentar o outro
@@ -62,11 +65,7 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn(
-			'2: Use peacock feathers to make a fly',
-			[row.text for row in rows]
-		)
-		
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 
 		# Edith se pergunta se o site lembrárá da sua lista. Então ela nota
